d5c603b5b3cbb2e88de4bc3856ec054e3a1715a2
joao.camara <joao.camara@mobicare.com.br>
2022-03-10 13:00:52 -0300

    estruturando app

---
+++ app/src/main/java/br/com/truckhospital/modules/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.truckhospital.modules.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.truckhospital.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/br/com/truckhospital/modules/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package br.com.truckhospital.modules.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.truckhospital.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
