0c1d61d904a1c5382a5e105ebe62b3bc31138d3e
joao.camara <joao.camara@mobicare.com.br>
2022-03-12 01:10:31 -0300

    adicionando dependencias

--- app/src/main/java/br/com/truckhospital/modules/splash/SplashActivity.kt
+++ app/src/main/java/br/com/truckhospital/modules/splash/SplashActivity.kt
@@ -3,11 +3,18 @@ package br.com.truckhospital.modules.splash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.truckhospital.R
+import br.com.truckhospital.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.splashLoginBtn.setOnClickListener {
+
+        }
     }
 }
