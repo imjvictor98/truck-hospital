2a7ffeec5e766b2dd216db1b9574e11070e5f43a
João Victor <47538285+imjvictor98@users.noreply.github.com>
2022-06-30 23:53:30 -0300

    removendo utils em desuso

--- app/src/main/java/br/com/truckhospital/modules/util/extension/DoubleExt.kt
+++
@@ -1,9 +0,0 @@
-package br.com.truckhospital.modules.util.extension
-
-import kotlin.math.pow
-import kotlin.math.roundToInt
-
-fun Double.roundTo(numFractionDigits: Int): Double {
-    val factor = 10.0.pow(numFractionDigits.toDouble())
-    return (this * factor).roundToInt() / factor
-}
